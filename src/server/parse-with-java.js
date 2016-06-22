5bb5e16a99749cb64ce749b5b91d919eb8208732
Matt Novinger <mnovinger@rallydev.com>
2016-06-22 10:27:19 -0600

    parsing with java then assembling with nodejs seems to work

---
+++ src/server/parse-with-java.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const _ = require('lodash');
+const util = require('util');
+const Q = require('q');
+const siteSource = require('./resources/campsite-names.json');
+
+const statusRegex = "(FULL|\\d|NA)";
+const initialStatusRegex = statusRegex + statusRegex + statusRegex + statusRegex + statusRegex + statusRegex;
+const siteIdRegex = "([\\d]{2,3}[A-Za-z]{0,2}-?\\d?)";
+const lastSnowDateRegex = "(\\d\\d?/\\d\\d)";
+const statusLineRegex = "^" + initialStatusRegex +  lastSnowDateRegex+"\\d+.?\\d?\\w+\\s?[[A-Za-z ()/-]+" + siteIdRegex + "\\s"+statusRegex+"$";
+const dateRegex ="(\\d{1,2}\\/\\d{1,2}\\/\\d{4})";
+const dateLineRegex = "^"+dateRegex+dateRegex+dateRegex+dateRegex+dateRegex+dateRegex+"\\s"+dateRegex+"$";
+
+function readFile(fileName) {
+    var deferred = Q.defer();
+    //noinspection JSUnresolvedFunction
+    fs.readFile(fileName, "utf-8", function (error, text) {
+        if (error) {
+            deferred.reject(new Error(error));
+        } else {
+            deferred.resolve(text);
+        }
+    });
+    return deferred.promise;
+}
+//java -jar pdfbox-app-2.0.2.jar ExtractText -console ~/personal-projects/rmnp-camping/resources/campsite_availability_list-06-16-2016.pdf
+
+readFile("./resources/campsite_availability_list-06-21-2016.txt").then((data) => {
+    const cgSites = _.map(siteSource, (site) => {
+        site.status = [];
+        return site;
+    });
+    const allDates = [];
+
+    /*
+    Parse file and pull out campsite status and the dates; its all jumbled.
+     */
+    const lines = data.split('\n');
+    _.each(lines, (line) => {
+        const status = line.match(statusLineRegex);
+        if (status) {
+            const site = cgSites.find((site) => {return site.id === status[8]});
+            if (!site) {
+                console.error(`Could not find site: ${status[8]}`)
+            } else {
+                site.status.push(status.slice(1,7).concat(status[9]));
+                site['last_snow'] = status[7];
+            }
+        } else {
+            const dates = line.match(dateLineRegex);
+            if (dates) {
+                Array.prototype.push.apply(allDates, dates.slice(1,8));
+            }
+        }
+    });
+
+    /*
+    transform the array of statuses into statuses with dates.
+     */
+    _.each(cgSites, (site) => {
+        var dateIdx = 0;
+        site.status = _.reduce(_.flatten(site.status), (acc, status) => {
+            acc.push({date: allDates[dateIdx], status: status})
+            dateIdx++;
+            return acc;
+        },[])
+    });
+
+
+    // should be 21 weeks worth
+    console.log(cgSites[0].status.length == 21);
+    console.log(util.inspect(cgSites[0]));
+    debugger;
+}, console.error);
