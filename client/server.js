6f02efdef04c60fee6c9c78ef1b676c62c89a4f7
Matt Novinger <mnovinger@rallydev.com>
2016-06-30 13:06:20 -0600

    adding validation for json data

--- client/server.js
+++ client/server.js
@@ -1,10 +1,13 @@
 // Create a basic Hapi.js server
 require('babel-register')({
-    presets: ['es2015', 'react'],
+    presets: ['es2015', 'react']
 });
 var Hapi = require('hapi');
 var dateFormat = require('dateformat');
 var format = "dd mmm HH:MM:ss";
+var util = require('util');
+
+var campsiteAvailability = require('./campsite-status.json');
 
 // Basic Hapi.js connection stuff
 var server = new Hapi.Server();
@@ -13,6 +16,41 @@ server.connection({
     port: process.env.PORT || 8000
 });
 
+var sanityCheckAvailability = (data) => {
+    /*
+     should have cgSiteData array with 139 entries and
+     allDates array with 133 entries
+
+     each cgSiteData.status should have the same number
+     of entries as allDates (133)
+     */
+
+    var isValid = true;
+    if (!data.cgSiteData) {
+        console.error("didn't find cgSiteData in availability file");
+        isValid = false;
+    }
+    if (data.cgSiteData.length != 139) {
+        console.error(`wrong number of site in cgSiteData ${data.cgSiteData.length} not 139`);
+        isValid = false;
+    }
+    if (!data.allDates) {
+        console.error("didn't find allDates in availability file");
+        isValid = false;
+    }
+    var dateCount = data.allDates.length;
+
+
+
+    return isValid;
+};
+
+if (!sanityCheckAvailability(campsiteAvailability)) {
+    console.error('found issues with the campsite availability, bailing');
+    process.exit(1);
+} else {
+    console.log('campsite availability file looks good');
+}
 // Register the inert and vision Hapi plugins
 // As of Hapi 9.x, these two plugins are no longer
 // included in Hapi automatically
@@ -21,7 +59,7 @@ server.register([{
     register: require('inert')
 }, {
     register: require('vision')
-}], function(err) {
+}], function (err) {
 
     if (err) return console.error(err);
 
@@ -32,6 +70,15 @@ server.register([{
         },
         relativeTo: __dirname,
         path: 'src/views'
+    });
+
+    // Add a route to serve static assets (CSS, JS, IMG)
+    server.route({
+        method: 'GET',
+        path: '/campsite-availability',
+        handler: (request, reply) => {
+            reply(campsiteAvailability);
+        }
     });
 
     // Add a route to serve static assets (CSS, JS, IMG)
@@ -55,7 +102,16 @@ server.register([{
         }
     });
 
-    server.start(function() {
+    server.route({
+        method: 'POST',
+        path: '/update-campsite-availability',
+        handler: (request, reply) => {
+            console.log('got it ' + util.inspect(request.payload));
+            reply('thanks!');
+        }
+    });
+
+    server.start(function () {
         console.log(dateFormat(new Date(), format) + ' - Server started at: ' + server.info.uri);
     });
 
