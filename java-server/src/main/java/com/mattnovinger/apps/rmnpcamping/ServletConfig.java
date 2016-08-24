fafd694615e1046386d0ceb3431403d05d8d6b18
Matt Novinger <mnovinger@rallydev.com>
2016-08-24 17:26:15 -0600

    fixed server port config

--- java-server/src/main/java/com/mattnovinger/apps/rmnpcamping/ServletConfig.java
+++ java-server/src/main/java/com/mattnovinger/apps/rmnpcamping/ServletConfig.java
@@ -1,15 +1,38 @@
 package com.mattnovinger.apps.rmnpcamping;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 
-@Controller
+@Configuration
+@EnableAutoConfiguration
 public class ServletConfig {
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
         return (container -> {
-            container.setPort(8012);
+            String envPort = System.getenv("PORT");
+            if (StringUtils.isEmpty(envPort)) {
+                envPort = "8182";
+            }
+            container.setPort(Integer.valueOf(envPort));
         });
     }
+
+//
+//
+//    public class AppConfig {
+//
+//        @Bean
+//        public EmbeddedServletContainerFactory servletContainer() {
+//            TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+//            factory.setPort(9000);
+//            factory.setSessionTimeout(10, TimeUnit.MINUTES);
+//            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+//            return factory;
+//        }
+//
+//    }
 }
