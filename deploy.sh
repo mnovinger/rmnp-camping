17a93b01494bd5052a6fee7a9b8d7ed2e76c5fb4
Matt Novinger <mnovinger@rallydev.com>
2016-08-25 11:54:04 -0600

    pushing

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-# if [ -z "$1" ]
-#   then
-#    echo "No commit message supplied"
-#    exit 1
-#fi
+ if [ -z "$1" ]
+   then
+    echo "No commit message supplied"
+    exit 1
+fi
 cd client
 ./build.sh
 cd ..
 cd java-server
 ./build.sh
 cd ..
-#git add .
-#git commit -m "${1}"
-#git subtree push --prefix client  heroku-frontend master
-#git subtree push --prefix java-server  heroku-backend master
-heroku logs -t --app rmnp-camping-backend
+git add .
+git commit -m "${1}"
+git subtree push --prefix client  heroku-frontend master
+git subtree push --prefix java-server  heroku-backend master
+#heroku logs -t --app rmnp-camping-backend
